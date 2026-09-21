684909998d46ea94935eaf76401db49455b06535
andreasmerkle1971 <a.merkle@evoinfo.de>
2026-09-21 17:15:26 +0200

    JSON structure

---
+++ scritp.js
@@ -0,0 +1,13 @@
+let questions =[
+    {
+        "question": "Wer hat HTML erfunden",
+        "answer_1": "Robbi Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners Lee",
+        "answer_4": "Justin Biber",
+        "right_answer": 3    
+    
+    
+    
+    },
+];
