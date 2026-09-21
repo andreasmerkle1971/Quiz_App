4be67e205352d7c0095d5a3bb1b9071322cbe77c
andreasmerkle1971 <a.merkle@evoinfo.de>
2026-09-21 22:53:30 +0200

    question rendered

--- scritp.js
+++ scritp.js
@@ -1,60 +1,75 @@
 let questions = [
     {
-        "question": "Wer hat HTML erfunden?",
-        "answer_1": "Robbi Williams",
-        "answer_2": "Lady Gaga",
-        "answer_3": "Tim Berners Lee",
-        "answer_4": "Justin Biber",
-        "right_answer": 3
+        question: "Wer hat HTML erfunden?",
+        answer_1: "Robbi Williams",
+        answer_2: "Lady Gaga",
+        answer_3: "Tim Berners Lee",
+        answer_4: "Justin Biber",
+        right_answer: 3,
     },
-        {
-        "question": "Was bedeutet das HTML Tag &lt;a&gt;?",
-        "answer_1": "Text Fett",
-        "answer_2": "Containera",
-        "answer_3": "Ein Link",
-        "answer_4": "Kursiv",
-        "right_answer": 3
+    {
+        question: "Was bedeutet das HTML Tag &lt;a&gt;?",
+        answer_1: "Text Fett",
+        answer_2: "Containera",
+        answer_3: "Ein Link",
+        answer_4: "Kursiv",
+        right_answer: 3,
     },
-        {
-        "question": "Wie bindet man eine Website in eine Website ein?",
-        "answer_1": "&lt;iframe&gt;, &lt;frame&gt;, and &lt;frameset&gt;",
-        "answer_2": "&lt;iframe&gt",
-        "answer_3": "&lt;frame&gt",
-        "answer_4": "&lt;frameset&gt",
-        "right_answer": 2
+    {
+        question: "Wie bindet man eine Website in eine Website ein?",
+        answer_1: "&lt;iframe&gt;, &lt;frame&gt;, and &lt;frameset&gt;",
+        answer_2: "&lt;iframe&gt",
+        answer_3: "&lt;frame&gt",
+        answer_4: "&lt;frameset&gt",
+        right_answer: 2,
     },
-        {
-        "question": "Wer hat HTML erfunden",
-        "answer_1": "Robbi Williams",
-        "answer_2": "Lady Gaga",
-        "answer_3": "Tim Berners Lee",
-        "answer_4": "Justin Biber",
-        "right_answer": 3
+    {
+        question: "Wer hat HTML erfunden",
+        answer_1: "Robbi Williams",
+        answer_2: "Lady Gaga",
+        answer_3: "Tim Berners Lee",
+        answer_4: "Justin Biber",
+        right_answer: 3,
     },
-            {
-        "question": "Welches Attribut kann man NICHT für Textarea verwenden?",
-        "answer_1": "readonly                                  ",
-        "answer_2": "max",
-        "answer_3": "from",
-        "answer_4": "spellscheck",
-        "right_answer": 1
+    {
+        question: "Welches Attribut kann man NICHT für Textarea verwenden?",
+        answer_1: "readonly                                  ",
+        answer_2: "max",
+        answer_3: "from",
+        answer_4: "spellscheck",
+        right_answer: 1,
     },
-                {
-        "question": "Wie wählst du alle Elemente vom Typ &lt;a&gt; mit dem attribut title aus?",
-        "answer_1": "a[title]{...}",
-        "answer_2": "a > title {...}",
-        "answer_3": "a.title {...}",
-        "answer_4": "a=title {...}",
-        "right_answer": 1
+    {
+        question:
+            "Wie wählst du alle Elemente vom Typ &lt;a&gt; mit dem attribut title aus?",
+        answer_1: "a[title]{...}",
+        answer_2: "a > title {...}",
+        answer_3: "a.title {...}",
+        answer_4: "a=title {...}",
+        right_answer: 1,
     },
-                {
-        "question": "Wie definiert man in JavaScript eine Variable?",
-        "answer_1": "let 100 = rate;",
-        "answer_2": "100 = let rate;",
-        "answer_3": "rate = 100;",
-        "answer_4": "let rate = 100;",
-        "right_answer": 4
-    }
-
-    
+    {
+        question: "Wie definiert man in JavaScript eine Variable?",
+        answer_1: "let 100 = rate;",
+        answer_2: "100 = let rate;",
+        answer_3: "rate = 100;",
+        answer_4: "let rate = 100;",
+        right_answer: 4,
+    },
 ];
+
+let currentQuestion = 0;
+
+function init(){
+    document.getElementById("all-questions").innerHTML = questions.length;
+    showQuestion();
+}
+
+function showQuestion(){
+    let question = questions[currentQuestion];
+    document.getElementById('questionText').innerHTML = question['question'];
+
+
+
+
+}
