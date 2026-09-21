c4963e3c06040701d5225b6eb20e5a2ed1b576fa
andreasmerkle1971 <a.merkle@evoinfo.de>
2026-09-21 22:57:40 +0200

    answer rendered

--- scritp.js
+++ scritp.js
@@ -60,16 +60,16 @@ let questions = [
 
 let currentQuestion = 0;
 
-function init(){
+function init() {
     document.getElementById("all-questions").innerHTML = questions.length;
     showQuestion();
 }
 
-function showQuestion(){
+function showQuestion() {
     let question = questions[currentQuestion];
-    document.getElementById('questionText').innerHTML = question['question'];
-
-
-
-
+    document.getElementById("questionText").innerHTML = question["question"];
+    document.getElementById("answer_1").innerHTML = question["answer_1"];
+     document.getElementById("answer_2").innerHTML = question["answer_2"];
+      document.getElementById("answer_3").innerHTML = question["answer_3"];
+       document.getElementById("answer_4").innerHTML = question["answer_4"];
 }
