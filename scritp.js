aeb121b69f71ca65811449b7eeaec41ccb43fbed
andreasmerkle1971 <a.merkle@evoinfo.de>
2026-09-22 15:17:32 +0200

    button definition

--- scritp.js
+++ scritp.js
@@ -69,7 +69,15 @@ function showQuestion() {
     let question = questions[currentQuestion];
     document.getElementById("questionText").innerHTML = question["question"];
     document.getElementById("answer_1").innerHTML = question["answer_1"];
-     document.getElementById("answer_2").innerHTML = question["answer_2"];
-      document.getElementById("answer_3").innerHTML = question["answer_3"];
-       document.getElementById("answer_4").innerHTML = question["answer_4"];
+    document.getElementById("answer_2").innerHTML = question["answer_2"];
+    document.getElementById("answer_3").innerHTML = question["answer_3"];
+    document.getElementById("answer_4").innerHTML = question["answer_4"];
+}
+
+function answer(selection){
+
+console.log('Selcted answer is ', selection);
+
+
+
 }
